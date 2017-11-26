576eff773fe6c51d858ff02671e468d2f89b82b0
Will Sharp <will.sharp@g5searchmarketing.com>
2017-11-26 15:59:08 -0800

    the real initial commit for RSpec exercise app

---
+++ app/api.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+require 'json'
+
+module ExpenseTracker
+	class API < Sinatra::Base
+
+		post '/expenses' do
+			JSON.generate('expense_id' => 42)
+		end
+
+		get '/expenses/:date' do
+			JSON.generate([])
+		end
+
+	end
+end
